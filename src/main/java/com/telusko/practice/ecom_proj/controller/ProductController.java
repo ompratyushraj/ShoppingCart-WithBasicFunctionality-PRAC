2cc8b4b1b51d4cc1b5ac5e50b9bdc419824bd469
Pratyush Raj Srivastava <ompratyushraj@gmail.com>
2024-10-23 00:00:32 +0530

    video22 - Solved the errors/

--- src/main/java/com/telusko/practice/ecom_proj/controller/ProductController.java
+++ src/main/java/com/telusko/practice/ecom_proj/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.telusko.practice.ecom_proj.controller;
 import com.telusko.practice.ecom_proj.model.Product;
 import com.telusko.practice.ecom_proj.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5174")
 public class ProductController {
 
     @Autowired
